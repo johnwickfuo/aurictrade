4f5897c9f9c6bf26cc851e5caca8cad4de668119
root <root@vmi2885903.contaboserver.net>
2026-05-15 14:00:14 +0200

    Initial upload

---
+++ storage/framework/views/0745c95986634547773d26bd303c3ad51cd9199b.php
@@ -0,0 +1,2 @@
+[<?php echo e($slot); ?>](<?php echo e($url); ?>)
+<?php /**PATH /home/admin/web/aurictrade.live/public_html/resources/views/vendor/mail/text/header.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/d601ad9faa9fb21124478991889e2b3924de2b0f.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH /home/admin/web/aurictrade.live/public_html/resources/views/vendor/mail/text/subcopy.blade.php ENDPATH**/ ?>
